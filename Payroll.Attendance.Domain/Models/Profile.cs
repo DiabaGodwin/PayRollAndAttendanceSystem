325ea1abd70b646dbaf8033d34c74113f19edf22
DIckson Oduro <od2511021@gmail.com>
2025-10-21 11:13:42 +0000

    pusing again

--- Payroll.Attendance.Domain/Models/Profile.cs
+++ Payroll.Attendance.Domain/Models/Profile.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Payroll.Attendance.Domain.Models
 {
-    internal class Profile
+    public class Profile
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB {  get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public User User { get; set; }
     }
 }
